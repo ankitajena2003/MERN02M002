83ccf339ecaa598caae92824abd067caf44cd236
ankitajena2003 <ankitajena2003>
2025-06-06 22:44:47 +0530

    done

---
+++ june/week1/day5/script.js
@@ -0,0 +1,172 @@
+// console.log("welcome")
+// // let a=1
+// // let A=2
+
+// // console.log(a)
+// // console.log(A)
+// let _="Qlith";
+// console.log(_)
+
+// let $="Qlith Innovation";
+// console.log($)
+
+//  let $name="xyz";
+//  console.log($name)
+
+//  let _name="y";
+//  console.log(_name);
+
+
+// let nameOfStudent="ankita";
+// console.log(nameOfStuden)
+
+// let a=20;
+// console.log(a)
+// console.log(typeof a)
+
+// let name="Qlith";
+// console.log(name,"type is",typeof name)
+
+// let isPass=true;
+// console.log(isPass)
+// console.log(typeof isPass)
+
+// let empty=undefined;
+// console.log(empty)
+// console.log(typeof empty)
+
+// let nullValue=null;
+// console.log(nullValue)
+// console.log(typeof nullValue)
+
+// let d=30.5;
+// console.log(typeof d)
+
+// //global declaration of the variable
+// var fullName="Qlith Innovation";
+// console.log(fullName)
+
+// {
+//     console.log(fullName)
+// }
+// function getName(){
+//     console.log(fullName)
+// }
+
+// getName()
+
+// //normal block
+// //this is my block level declaration of variable
+// {
+//     var a=40;
+//     console.log("blocklevel declaration",a)
+// }
+// console.log(fullName)
+
+//*This is the let variable declaration
+
+//global declaration
+// let name="innovation";
+// console.log(name)
+
+// {
+//     console.log(name)
+
+// }
+// function getName(){
+//     console.log(name)
+// }
+// getName()
+
+// //!Blocklevel declaration
+
+// {
+//     let a=30;
+//     console.log(a)
+// }
+
+//This is the let variable declaration
+
+//global variable
+// const name="xyz";
+// console.log(name)
+// {
+//     console.log(name)
+// }
+// function getName(){
+//     console.log(name)
+// }
+// {
+//     const a=70;
+//     console.log(a);
+
+// }
+// console.log(a)
+
+// var a=60;
+// console.log(a)
+
+// a=40;
+// console.log(a)
+
+// //dynamically typed language
+// a="y";
+// console.log(a)
+
+// var a=30;
+// console.log(a)
+// //there is no same variable name using let
+
+//~ Difference bet var,let,const
+
+//Redeclaration
+//var- possible
+//let-not possible
+//const-not
+
+//reinitialization
+//var- possible
+//let- possible
+//const-not
+
+//only declaration possible or not
+//var- possible
+//let- possible
+//const-not
+
+//way of declaration and Access
+//var
+//global declaration
+//local access:var-access
+//block access:access
+//function access:access
+
+//let 
+//global declaration
+//local access:let-access
+//block access:access
+//function access:access
+
+
+
+
+
+
+
+//block level declaration
+//local access:var-possible
+//block access:access
+//function access:access
+
+//blockdclaration
+//local access:var-access
+//block access:access
+//function access:access
+
+//function level declaration
+//local access:var- not possible
+//block access: Not
+//function access:access
+
+
+
